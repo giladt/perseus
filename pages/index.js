111e1ce4e5348fb65419e09459e24043a5eff077
Gilad Tsabar <gilad@tsabar.net>
2021-05-10 13:12:58 +0200

    init commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { useState, useEffect, useRef } from "react"
+
+const RICK_AND_MORTY_API = {
+  CHARACTERS_URI: 'https://rickandmortyapi.com/api/character',
+  EPISODES_URI: 'https://rickandmortyapi.com/api/episode',
+  LOCATIONS_URI: 'https://rickandmortyapi.com/api/location',
+}
+
+const handleGetEpisodes = async ( ref ) => {
+  const character = ref.currentTarget.id
+  const character_episodes = await ( await fetch( `${ RICK_AND_MORTY_API.CHARACTERS_URI }/${ character }` ) ).json()
+
+  const episodes = character_episodes.episode.map( async ( episode ) => {
+    return await ( await ( await fetch( episode ) ).json() )
+  } )
+
+  console.log( episodes )
+  return await episodes
+}
+
+export default function Home ( { data } ) {
+
+  const [ characters, setCharacters ] = useState( [] )
+  const [ episodes, setEpisodes ] = useState( [] )
+
+  const ref = useRef( null )
+
+  if ( !data ) return <div>Loading...</div>
+  useEffect( () => {
+    setCharacters( data.results )
+  }, [] )
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +44,66 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Rick and Morty Episode selector
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by selecting your favorite character
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <section className={ styles.grid }>
+          { characters.map( ( character ) => {
+            return (
+              <div key={ character.id } ref={ ref } id={ character.id } className={ styles.card } onClick={ async ( e ) => setEpisodes( await handleGetEpisodes( e ) ) }>
+                <img className={ styles.logo } src={ character.image } alt={ character.name } />
+                { character.name }
+              </div>
+            )
+          } ) }
+        </section>
+        <section className={ `${ styles.grid } ${ styles.episodes }` }>
+          { episodes?.results?.length > 0 && episodes.results.map( ( episode ) => {
+            return (
+              <div key={ episode.id } className={ styles.card }>
+                <h1>{ episode.name }</h1>
+                <b>{ episode.air_date }</b>
+                <p>{ episode.episode }</p>
+              </div>
+            )
+          } ) }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        </section>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.perseus.de/en/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Coding Challenge made by Gilad Tsabr, for{ ' ' }
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/perseus.svg" alt="Perseus Logo" width={ 72 } height={ 16 } />
           </span>
         </a>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps ( context ) {
+  const res = await fetch( RICK_AND_MORTY_API.CHARACTERS_URI )
+  const data = await res.json()
+
+  if ( !data ) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: { data },
+  }
 }
